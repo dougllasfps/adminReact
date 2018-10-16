1e644bedb122af37472a646e07a51e17f2802d11
Dougllas Sousa <dougllasfps@gmail.com>
2018-10-15 23:21:35 -0300

    implementando templates de listagem e formulario de cadsatro.

--- src/main/resources/src/components/permissao/PermissaoForm.jsx
+++ src/main/resources/src/components/permissao/PermissaoForm.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Field, reduxForm} from 'redux-form'
 
+import DefaultForm from '../../components/templates/DefaultForm'
+
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import {Button} from "primereact/button";
 import {cancel} from './permissaoActions'
 
 import {update_mode, insert_mode} from '../../main/constants/constants'
@@ -18,39 +19,20 @@ class PermissaoForm extends React.Component{
         let pageTitle = pageMode === update_mode ? 'Atualização de Permissão' : 'Cadastro de Permissão';
 
         return (
-            <form onSubmit={this.props.handleSubmit}>
-                <Field component="input" type="hidden" name="id" />
-                <div className="p-grid p-fluid">
-                    <div className="p-col-12 p-lg-12">
-                        <div className="card card-w-title">
-                            <h1>{pageTitle}</h1>
-                            <br />
-                            <div className="p-grid">
-                                <div className="p-md-6">
-                                    <span className="p-float-label">
-                                        <Field id="inputDesc" component="input" name="descricao" className="p-inputtext p-component"/>
-                                        <label htmlFor="inputDesc">Descrição</label>
-                                    </span>
-                                </div>
-                                <div className="p-md-6">
-                                    <span className="p-float-label">
-                                        <Field id="inputLabel"  component="input" name="label"  className="p-inputtext p-component"/>
-                                        <label htmlFor="inputLabel">Label</label>
-                                    </span>
-                                </div>
-                                <div className="p-md-2">
-                                    <Field type="submit" component="button" name="submitButton" className="p-button p-component p-button-secondary p-button-text-only" >
-                                        <span className="p-button-text p-c">{submitLabel}</span>
-                                    </Field>
-                                </div>
-                                <div className="p-md-2">
-                                    <Button label="Cancelar" className="p-button-danger" onClick={() => this.props.cancel()} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <DefaultForm handleCancel={() => this.props.cancel()} pageTitle={pageTitle} submitLabel={submitLabel} handleSubmit={this.props.handleSubmit}>
+                <div className="p-md-6">
+                    <span className="p-float-label">
+                        <Field id="inputDesc" component="input" name="descricao" className="p-inputtext p-component"/>
+                        <label htmlFor="inputDesc">Descrição</label>
+                    </span>
                 </div>
-            </form>
+                <div className="p-md-6">
+                    <span className="p-float-label">
+                        <Field id="inputLabel"  component="input" name="label"  className="p-inputtext p-component"/>
+                        <label htmlFor="inputLabel">Label</label>
+                    </span>
+                </div>
+            </DefaultForm>
         )
     }
 }
---
+++ src/main/resources/src/components/templates/DefaultList.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import {Button} from "primereact/button";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import {Dialog} from "primereact/dialog";
+
+export default class DefaultList extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            confirmDelete: false,
+            idEntidade: null
+        };
+
+        this.actionButtons = this.actionButtons.bind(this);
+        this.showDelete = this.showDelete.bind(this);
+        this.hideDelete = this.hideDelete.bind(this);
+        this.prepareDelete = this.prepareDelete.bind(this);
+        this.confirmDelete = this.confirmDelete.bind(this);
+    }
+
+    actionButtons(rowData, column) {
+        return (
+            <div>
+                <Button type="button" icon="pi pi-pencil" className="p-button-secondary" onClick={() => this.props.prepareEditar(rowData)}  />
+                <Button type="button" icon="pi pi-trash"  className="p-button-danger" onClick={() => this.prepareDelete(rowData) } />
+            </div>
+        )
+    }
+
+    prepareDelete(rowData){
+        this.setState({...this.state, idEntidade:rowData.id })
+        this.showDelete()
+    }
+
+    showDelete(event) {
+        this.setState({confirmDelete: true});
+    }
+
+    confirmDelete(){
+        this.props.remove(this.state.idEntidade)
+        this.hideDelete()
+    }
+
+    hideDelete(event) {
+        this.setState({confirmDelete: false});
+    }
+
+    render(){
+
+        const confirmDeleteFooter = (
+            <div>
+                <Button label="Confirma" icon="pi pi-check" onClick={this.confirmDelete} className="p-button-secondary" />
+                <Button label="Cancela"  icon="pi pi-times" onClick={this.hideDelete} className="p-button-danger" />
+            </div>
+        );
+
+        let colunaAcoes = {body: this.actionButtons, header: '',className: 'colunaAcoes', field : 'acao'};
+
+        let cols = this.props.columns || [];
+        cols.push(colunaAcoes)
+
+        let dynamicColumns = cols.map((col,i) => {
+            return <Column key={col.field} field={col.field} header={col.header} body={col.body} className={col.className} />;
+        });
+
+        return(
+            <div className="p-grid p-fluid">
+                <div className="p-col-12 p-lg-12">
+                    <div className="card card-w-title">
+                        <h1>Permissões</h1>
+                        <div className="p-grid">
+                            <div className="p-md-10" />
+                            <div className="p-md-2">
+                                <Button label="Novo" icon="pi pi-plus" onClick={this.props.prepareInsert}/>
+                            </div>
+                            <div className="p-md-12">
+                                <DataTable value={this.props.list}
+                                           paginator={true}
+                                           rows={10}
+                                           rowsPerPageOptions={[5,10,20]} >
+                                    {dynamicColumns}
+                                </DataTable>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Dialog header="Confirmação"
+                        visible={this.state.confirmDelete}
+                        width="350px"
+                        modal={true}
+                        footer={confirmDeleteFooter}
+                        minY={70}
+                        onHide={this.hideDelete}
+                        maximizable={true}
+                        blockScroll={true}>
+                    Deseja deletar o registro?
+                </Dialog>
+
+            </div>
+        )
+    }
+}
